39967c0de1fe81973d4119a70db91fe5e802e520
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2024-01-13 13:20:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || "0.0.0.0";
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
+let allowedOrigin = process.env.allowedOrigin || "http://localhost:1234";
+
+import { createServer } from "cors-anywhere";
+
+createServer({
+  originWhitelist: [allowedOrigin], // Allow all origins
+}).listen(port, host, function () {
+  console.log("Running CORS Anywhere on " + host + ":" + port);
+});
